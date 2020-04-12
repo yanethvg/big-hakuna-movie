d620c1945e291da4073bd00083ac17d27daa7bed
Zoila <zoilavillatoro6694@outlook.com>
2020-04-12 09:59:00 -0600

    agregado estilos a cards de Pelicula

--- src/components/Peliculas/Pelicula.jsx
+++ src/components/Peliculas/Pelicula.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Pelicula = ({ pelicula }) => {
     return (
         <React.Fragment>
-            <div className="card">
+            <div className="card border-dark" style={{boxShadow: '10px 10px 5px grey'}}>
                 <img src={
                      pelicula.poster_path 
                         ? `${URL_BASE_IMG}${pelicula.poster_path}` 
@@ -15,7 +15,8 @@ const Pelicula = ({ pelicula }) => {
                     <p className="card-text">{pelicula.overview.substring(0, 50)}...</p>
                     <p className="card-text"><small className="text-muted">Release: {pelicula.release_date}</small></p>
                 </div>
-                <Link to={`/movies/${pelicula.id}`} type="button" className="btn btn-outline-danger ">See More</Link>
+                <Link type="button" class="btn btn-outline-danger my-2 mx-2">Rent</Link>
+                <Link to={`/movies/${pelicula.id}`} type="button" className="btn btn-outline-dark my-2 mx-2">See More</Link>
             </div>
         </React.Fragment>
     );
